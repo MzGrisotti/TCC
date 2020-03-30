192240952ef81ba5416d4b19042063a20cd08a42
mzgrisotti <mzgrisotti@inf.ufsm.br>
2020-03-30 03:53:44 -0300

    Added Web3 Connection

--- eBPF/monitor.py
+++ eBPF/monitor.py
@@ -3,6 +3,12 @@ from bcc.utils import printb
 from ctypes import *
 from Flow_Class import Flow_Data
 from Helper_Functions import *
+
+import json
+import time
+import random
+from web3 import Web3
+
 import time
 import sys
 import uptime
@@ -47,9 +53,21 @@ def load_ebpf_program():
 
     return bpf
 
+def download_blockchain_data():
+    web3 = Web3(Web3.HTTPProvider('HTTP://10.0.0.112:7545'))
+
+    abi = json.loads('[ { "constant": true, "inputs": [ { "name": "", "type": "address" }, { "name": "", "type": "uint256" } ], "name": "Flows_id", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "constant": false, "inputs": [ { "name": "_monitor", "type": "address" } ], "name": "Set_Monitor", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_host", "type": "string" }, { "name": "_destiny", "type": "string" }, { "name": "_protocol", "type": "string" } ], "name": "New_Flow", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_id", "type": "uint256" } ], "name": "Increase_Count", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Id", "outputs": [ { "name": "", "type": "uint256[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_id", "type": "uint256" } ], "name": "Get_Flow", "outputs": [ { "name": "", "type": "uint256" }, { "name": "", "type": "string" }, { "name": "", "type": "string" }, { "name": "", "type": "string" }, { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Qnt", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" } ]')
+
+    monitor = '0x5267D97e8C44fd7a3D8FccC484b5038e39fa4b31'
+    contract_address = '0x16C8730b5abcaAa78457CE58a92775d7af9e5ed8'
+    address = web3.toChecksumAddress(contract_address)
+    contract = web3.eth.contract(address = address, abi=abi)
+
+
 def main():
 
     bpf = load_ebpf_program()
+    download_blockchain_data()
     main_loop(bpf)
     # debug(bpf)
 
