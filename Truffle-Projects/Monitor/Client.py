4f882d1c15078b059db617f66f49777dec1a48a0
Matheus Grisotti <mzgrisotti@inf.ufsm.br>
2020-03-30 02:57:17 -0300

    Updated Account Addresses

--- Truffle-Projects/Monitor/Client.py
+++ Truffle-Projects/Monitor/Client.py
@@ -1,22 +1,22 @@
 import json
 from web3 import Web3
 
-web3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
+web3 = Web3(Web3.HTTPProvider('HTTP://10.0.0.112:7545'))
 
 abi = json.loads('[ { "constant": true, "inputs": [ { "name": "", "type": "address" }, { "name": "", "type": "uint256" } ], "name": "Flows_id", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "constant": false, "inputs": [ { "name": "_monitor", "type": "address" } ], "name": "Set_Monitor", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_host", "type": "string" }, { "name": "_destiny", "type": "string" }, { "name": "_protocol", "type": "string" } ], "name": "New_Flow", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_id", "type": "uint256" } ], "name": "Increase_Count", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Id", "outputs": [ { "name": "", "type": "uint256[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_id", "type": "uint256" } ], "name": "Get_Flow", "outputs": [ { "name": "", "type": "uint256" }, { "name": "", "type": "string" }, { "name": "", "type": "string" }, { "name": "", "type": "string" }, { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Qnt", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" } ]')
 
-owner = '0xf631b1cC9CcF165eC0C161E4B85F03E68A081bef'
-monitor = '0x9f963803EFAC3592ad63a1cD3ca736c9Eed5b9c2'
+owner = '0xa803db32a7a3034F93478022Fcf9e58D96A840A8'
+monitor = '0x5267D97e8C44fd7a3D8FccC484b5038e39fa4b31'
 
-client1 = '0x9aEFb1F7DEdb7F9a1aC578B3Be7c63Db33b1c78b'
-client2 = '0xe55A3Dc7a8947578dAd1B133175E45a873eF993e'
-client3 = '0x75B15B69D98E577d6509bd5E7C3285C138A2d7ab'
-client4 = '0x4B2D9Dc47a32956aEe120fa4df1Ab59f0F8FDC78'
-client5 = '0xEF8Bdd60fcC4f35cc9BA47c754bAE0358d8224D8'
+client1 = '0xB0439dAecDf803925dc37531dcbc8e9Ff4A97A6a'
+client2 = '0x95BFa40fEe133E15e1379BC21aF554Fb94FedA91'
+client3 = '0x1Aa329ed01CDfB77EC1d01d0Ecc234d0c40EC37f'
+client4 = '0x5d28D02d031900Ef18A8653490cE99dD2B70df15'
+client5 = '0x3eECC2bc23ed68d13A3459115599271a6183F182'
 
 clients = [client1,client2,client3,client4,client5]
 
-address = web3.toChecksumAddress('0x67784810D0C87E4eCd31C6beA38E2ac318BA97F4')
+address = web3.toChecksumAddress('0x16C8730b5abcaAa78457CE58a92775d7af9e5ed8')
 contract = web3.eth.contract(address = address, abi=abi)
 
 tx_hash = contract.functions.Set_Monitor(monitor).transact({'from': owner})
