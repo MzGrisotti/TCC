30183e8af9b83c0f3d00e2efa9ee1aea49cc3030
Matheus Grisotti <mzgrisotti@inf.ufsm.br>
2020-03-30 19:33:43 -0300

    Updated Flow Class

--- Truffle-Projects/Monitor/contracts/Monitor.sol
+++ Truffle-Projects/Monitor/contracts/Monitor.sol
@@ -4,11 +4,16 @@ contract Monitor{
 
     struct Flow{
         uint id;
+        address client;
         string host;
         string destiny;
-        string protocol;
-        address client;
-        uint count;
+        uint protocol;
+        uint256 pktcount;
+        uint256 total_bytes;
+        uint256 start_tstamp;
+        uint256 end_tstamp;
+        uint256 last_packet_tstamp;
+        uint256 duration;
     }
 
     mapping(uint => Flow) private Flows;
@@ -29,31 +34,36 @@ contract Monitor{
 
     }
 
-    function New_Flow(string memory _host, string memory _destiny, string memory _protocol) public{
+    function New_Flow(string memory _host, string memory _destiny, uint _protocol) public{
         uint actual = flows_qnt;
         flows_qnt++;
         Flows_id[msg.sender].push(actual);
         Flows[actual].id = actual;
+        Flows[actual].client = msg.sender;
+
+        //Set by client
         Flows[actual].host = _host;
         Flows[actual].destiny = _destiny;
-        Flows[actual].count = 0;
         Flows[actual].protocol = _protocol;
-        Flows[actual].client = msg.sender;
-    }
 
-    function Increase_Count(uint _id) public{
-        require(msg.sender == monitor, "Esse metodo so pode ser acessado pelo monitor");
-        Flows[_id].count++;
+        //Set to default
+        Flows[actual].pktcount = 0;
+        Flows[actual].total_bytes = 0;
+        Flows[actual].start_tstamp = block.timestamp;
+        Flows[actual].end_tstamp = 0;
+        Flows[actual].last_packet_tstamp = 0;
+        Flows[actual].duration = 0;
+
     }
 
     function Get_Flow_Id() public view returns(uint[] memory){
         return Flows_id[msg.sender];
     }
 
-    function Get_Flow(uint _id) public view returns(uint, string memory, string memory, string memory, uint){
+    function Get_Flow(uint _id) public view returns(uint, string memory, string memory, uint, uint256, uint256, uint256, uint256, uint256){
         require(Flows[_id].client == msg.sender || msg.sender == monitor, "Esse dado so pode ser acessado pelo cliente a quem ele pertence");
         Flow memory f = Flows[_id];
-        return(f.id, f.host, f.destiny, f.protocol, f.count);
+        return(f.id, f.host, f.destiny, f.protocol, f.pktcount, f.total_bytes, f.start_tstamp, f.last_packet_tstamp, f.duration);
     }
 
     function Get_Flow_Qnt() public view returns(uint){
