2b04291e07ba5e6d595d016988e11e2a44f26753
mzgrisotti <mzgrisotti@inf.ufsm.br>
2020-04-13 01:07:00 -0300

    Added Merge Flow Descriptor to eBPF map on runtime

--- eBPF/monitor.py
+++ eBPF/monitor.py
@@ -2,7 +2,9 @@ from bcc import BPF
 from bcc.utils import printb
 from ctypes import *
 from Flow_Class import Flow_Data
+from Oracle_Class import Oracle
 from Helper_Functions import *
+from threading import Thread
 
 import json
 import time
@@ -14,20 +16,6 @@ import sys
 import uptime
 
 interface = "enp0s3"
-
-def new_key(bpf):
-    print("Creating New Key")
-    map = bpf.get_table("Flow")
-
-    flow = Flow_Data(map, "192.0.0.1", "80.101.30.20", 8080, 3999, 10, uptime.uptime())
-    key = flow.get_key()
-    leaf = flow.get_leaf()
-    map[key] = leaf
-
-    flow = Flow_Data(map, "127.0.0.1", "127.0.0.1", 9400, 9500, 17, uptime.uptime())
-    key = flow.get_key()
-    leaf = flow.get_leaf()
-    map[key] = leaf
 
 def load_ebpf_program():
 
@@ -52,7 +40,7 @@ def download_blockchain_data(bpf):
 
     web3 = Web3(Web3.HTTPProvider('HTTP://10.0.0.112:7545'))
 
-    abi = json.loads('[ { "constant": true, "inputs": [ { "name": "", "type": "address" }, { "name": "", "type": "uint256" } ], "name": "Flows_id", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "constant": false, "inputs": [ { "name": "_monitor", "type": "address" } ], "name": "Set_Monitor", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_host", "type": "string" }, { "name": "_destiny", "type": "string" }, { "name": "_protocol", "type": "uint256" }, { "name": "_hport", "type": "uint256" }, { "name": "_dport", "type": "uint256" } ], "name": "New_Flow", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_id", "type": "uint256" }, { "name": "_pktcnt", "type": "uint64" }, { "name": "_t_bytes", "type": "uint64" }, { "name": "_last_pkt_tstamp", "type": "uint64" }, { "name": "_end_tstamp", "type": "uint64" } ], "name": "Update_Flow", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Id", "outputs": [ { "name": "", "type": "uint256[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_id", "type": "uint256" } ], "name": "Get_Flow", "outputs": [ { "name": "", "type": "uint256" }, { "name": "", "type": "string" }, { "name": "", "type": "string" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Qnt", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" } ]')
+    abi = json.loads('[ { "constant": true, "inputs": [ { "name": "", "type": "address" }, { "name": "", "type": "uint256" } ], "name": "Flows_id", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": false, "name": "id", "type": "uint256" } ], "name": "new_entry", "type": "event" }, { "constant": false, "inputs": [ { "name": "_monitor", "type": "address" } ], "name": "Set_Monitor", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_host", "type": "string" }, { "name": "_destiny", "type": "string" }, { "name": "_protocol", "type": "uint256" }, { "name": "_hport", "type": "uint256" }, { "name": "_dport", "type": "uint256" } ], "name": "New_Flow", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "_id", "type": "uint256" }, { "name": "_pktcnt", "type": "uint64" }, { "name": "_t_bytes", "type": "uint64" }, { "name": "_last_pkt_tstamp", "type": "uint64" }, { "name": "_end_tstamp", "type": "uint64" } ], "name": "Update_Flow", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Id", "outputs": [ { "name": "", "type": "uint256[]" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_id", "type": "uint256" } ], "name": "Get_Flow", "outputs": [ { "name": "", "type": "uint256" }, { "name": "", "type": "string" }, { "name": "", "type": "string" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" }, { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "Get_Flow_Qnt", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" } ]')
 
     monitor = '0x5267D97e8C44fd7a3D8FccC484b5038e39fa4b31'
     contract_address = '0x16C8730b5abcaAa78457CE58a92775d7af9e5ed8'
@@ -64,9 +52,20 @@ def download_blockchain_data(bpf):
     Flows = dict()
     map = bpf.get_table("Flow")
 
+    oracle = Oracle(web3, Smart_Contract, map)
+    oracle.start()
+
     for i in range(last_flow_id):
         new_flow = Smart_Contract.functions.Get_Flow(i).call({'from':monitor})
         Flows[new_flow[0]] = Flow_Data(new_flow, Smart_Contract, map)
+
+    update_ebpf_map(bpf, Flows)
+
+    return web3, Smart_Contract, Flows, oracle
+
+def update_ebpf_map(bpf, Flows):
+
+    map = bpf.get_table("Flow")
 
     print("Exporting Flows to kernel memory")
     for key in Flows:
@@ -74,32 +73,33 @@ def download_blockchain_data(bpf):
         map_leaf = Flows[key].get_leaf()
         map[map_key] = map_leaf
 
-    return web3, Smart_Contract, Flows
-
-
 def main():
 
     bpf = load_ebpf_program()
-    web3, Smart_Contract, Flows = download_blockchain_data(bpf)
-    oracle = Oracle(web3, Smart_Contract)
-    block_filter = web3.eth.filter({'fromBlock':'latest', 'address':'0x16C8730b5abcaAa78457CE58a92775d7af9e5ed8'})
-    oracle.log_loop(block_filter, 2)
-    main_loop(bpf, Flows, web3, Smart_Contract)
+    web3, Smart_Contract, Flows, oracle = download_blockchain_data(bpf)
+
+    main_loop(bpf, Flows, web3, Smart_Contract, oracle)
 
 
-def export_all(map):
-    i = 0
+def export_all(Flows, web3, smart_contract):
+    for flow in Flows:
+        Flows[flow].export(web3, smart_contract)
 
-def main_loop(bpf, Flows, web3, Smart_Contract):
+
+def main_loop(bpf, Flows, web3, Smart_Contract, oracle):
     flow_data = bpf.get_table("Flow")
     info_data = bpf.get_table("Info")
-    print("Printing data")
+    print("Collecting Data")
     # new_key(bpf)
     export_time_limit = 300 #seconds
-    export_time = uptime.uptime()
+    last_export_tstamp = uptime.uptime()
     while 1:
-        if(uptime.uptime() - export_time > export_time_limit):
-            export_all(Flows)
+
+        new_flows = oracle.get_new_entries()
+        if(len(new_flows) > 0):
+            print("Merging New Flows")
+            update_ebpf_map(bpf, new_flows)
+            Flows.update(new_flows)
         try:
             print("\nFlows:\n")
             for k in flow_data.keys():
@@ -107,17 +107,17 @@ def main_loop(bpf, Flows, web3, Smart_Contract):
                 id = map.id
                 Flows[id].update_stats_from_collector(map)
                 a = Flows[id].verify_export(web3, Smart_Contract)
-                    # flow_data.__delitem__(k)
                 Flows[id].show()
-                # flow = Flow_Data(map)
-                # flow.show()
 
             time.sleep(1)
+
         except KeyboardInterrupt:
             print("Removing filter from device")
             break;
 
+    export_all(Flows, web3, Smart_Contract)
     bpf.remove_xdp(interface)
+    oracle.stop_t()
 
 
 if __name__ == "__main__":
